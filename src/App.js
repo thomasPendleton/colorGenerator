43f8f8af3620679f9b745555b993a53c3a0220ce
Thomas Pendleton <thomas6576@yahoo.com>
2022-06-15 11:14:23 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -4,26 +4,50 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  const [color, setColor] = useState('')
+  const [color, setColor] = useState('#f15025')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
+    setError(false)
     e.preventDefault()
-    console.log(color);
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (err) {
+      setError(true)
+      console.log(err)
+    }
   }
   return (
     <>
       <section className="container">
         <h3>Color generator</h3>
-        <form onSubmit={handleSubmit} >
-          <input type="text" value={color} placeholder='#f15025' onChange={(e) => setColor(e.target.value)}/>
-          <button className="btn" type='submit'>submit</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+            className={error ? 'error' : undefined}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
         </form>
       </section>
 
       <section className="colors">
-      <h4>list goes here</h4>
+        {list.map((item, idx) => {
+          return (
+            <SingleColor
+              key={idx}
+              {...item}
+              hexColor={item.hex}
+              idx={idx}
+            ></SingleColor>
+          )
+        })}
       </section>
     </>
   )
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, type, hexColor, idx }) => {
+  const [alert, setAlert] = useState(false)
+  const bcg = rgb.join(',')
+  const hexValue = `#${hexColor}`
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [alert])
+
+  //Unused rgb to hex function
+  const hex = rgbToHex(...rgb)
+
+  return (
+    <article
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
+      className={`color ${idx > 10 ? 'color-light' : undefined}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
